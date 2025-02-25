bc4e15bd4958791fd3b3a894e5367f6ca4dd33c6
ww-org <semir@shadowwhite.org>
2025-02-25 17:52:56 +0300

    Add: Base Stat classes

---
+++ hhg-case-archer/Assets/_Game/Scripts/StatSystem/Stat.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using _Game.Enums;
+
+namespace _Game.StatSystem
+{
+    public class Stat
+    {
+        public float BaseValue;
+        private List<StatModifier> _modifiers = new List<StatModifier>();
+    
+        public Stat(float baseValue)
+        {
+            BaseValue = baseValue;
+        }
+    
+        public float GetValue()
+        {
+            float finalValue = BaseValue;
+            float percentMultiplier = 1f;
+        
+            foreach (var mod in _modifiers)
+            {
+                if (mod.Type == ModifierType.Flat)
+                    finalValue += mod.Value;
+                else if (mod.Type == ModifierType.Percentage)
+                    percentMultiplier += mod.Value / 100f;
+            }
+            return finalValue * percentMultiplier;
+        }
+    
+        public void AddModifier(StatModifier modifier)
+        {
+            _modifiers.Add(modifier);
+        }
+    
+        public void RemoveModifier(StatModifier modifier)
+        {
+            _modifiers.Remove(modifier);
+        }
+    }
+}
